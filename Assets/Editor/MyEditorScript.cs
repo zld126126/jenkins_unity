4d99e0e789199b1748e4cf69862b4029fb6df69b
landon <zld126126@126.com>
2023-05-17 13:34:10 +0800

    update

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -1,13 +1,13 @@
+using System;
 using UnityEditor;
 
 class MyEditorScript {
         [MenuItem ("Custom/CI/Build Android")]
         public static void BuildAndroid()
         {
-            string[] scenes = { "Assets/Scenes/myJenkinsUnity.unity"}; // 填写您的场景文件路径
-
-            string outputPath = "Builds/Android/myJenkinsUnity.apk"; // 填写输出APK的路径和名称
-
+            string[] scenes = { "Assets/Scenes/SampleScene.unity"}; // 填写您的场景文件路径
+            string outputPath = "Builds/Android/JenkinsUnity.apk";
+            
             BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
         }
 }
